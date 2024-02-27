c081b66b402c7b41d05bb78ecdcadce7407d3f33
Emilio-Amaro <156143027+Emilio-Amaro@users.noreply.github.com>
2024-02-27 17:06:38 -0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,69 @@
+var path,boy,cash,diamonds,jwellery,sword;
+var pathImg,boyImg,cashImg,diamondsImg,jwelleryImg,swordImg;
+var treasureCollection = 0;
+var cashG,diamondsG,jwelleryG,swordGroup;
 
-cashG.destroyEach();
+//Game States
+var PLAY=1;
+var END=0;
+var gameState=1;
+
+function preload(){
+  pathImg = loadImage("Road.png");
+  boyImg = loadAnimation("Runner-1.png","Runner-2.png");
+  cashImg = loadImage("cash.png");
+  diamondsImg = loadImage("diamonds.png");
+  jwelleryImg = loadImage("jwell.png");
+  swordImg = loadImage("sword.png");
+  endImg =loadAnimation("gameOver.png");
+}
+
+function setup(){
+
+//crear el canvas y ajustar el tamaño de la ventana para que sea compatible con el dispositivo 
+
+createCanvas(windowWidth,windowHeight);
+
+path=createSprite(width/2,200);
+path.addImage(pathImg);
+path.velocityY = 4;
+
+
+//crear sprite boy corriendo
+boy = createSprite(width/2,height-20,20,20);
+boy.addAnimation("SahilRunning",boyImg);
+boy.scale=0.08;
+  
+  
+cashG=new Group();
+diamondsG=new Group();
+jwelleryG=new Group();
+swordGroup=new Group();
+
+}
+
+function draw() {
+
+  if(gameState===PLAY){
+  background(0);
+  boy.x = World.mouseX;
+  
+  edges= createEdgeSprites();
+  boy.collide(edges);
+  
+  //código para reiniciar el fondo
+
+  if(path.y > height ){
+    path.y = height/2;
+  }
+
+    createCash();
+    createDiamonds();
+    createJwellery();
+    createSword();
+
+    if (cashG.isTouching(boy)) {
+      cashG.destroyEach();
       treasureCollection=treasureCollection + 50;
     }
     else if (diamondsG.isTouching(boy)) {
@@ -90,4 +154,3 @@ function createSword(){
   swordGroup.add(sword);
   }
 }
-
